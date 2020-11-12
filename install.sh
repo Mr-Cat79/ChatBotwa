9489f045106dc962e8ede97ec720842b6c6c2b56
Mr-Cat79 <74307908+Mr-Cat79@users.noreply.github.com>
2020-11-12 08:09:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
+termux-setup-storage
 apt update
 apt upgrade
-apt install
-pkg update && pkg upgrade
+apt install git
 pkg install wget
 pkg install ffmpeg
 pkg install nodejs
+git clone https://github.com/Mr-Cat79/ChatBotwa
 npm i -g cwebp
 npm i -g ytdl 
 npm i
 npm i got
+node index.js
